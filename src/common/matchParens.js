7e00d084def95386b6bfe6df516d6239ff743119
chrisba <c.bailey1@uqconncet.edu.au>
2019-08-30 23:07:18 +1000

    Implement matchParens
    Convert from python to js and split from interpreter.js

---
+++ src/common/matchParens.js
@@ -0,0 +1,43 @@
+/*
+def matchParens(text,start,openStr,closeStr):
+    count=0
+    charNum=start
+    firstChar=True
+    while(count>0 or firstChar):
+        if(charNum>=len(text)):
+            #print "err:could not find match!"
+            return -1
+        elif(text[charNum]==closeStr):
+            count=count-1
+            #print "close at "+str(charNum)
+        elif(text[charNum]==openStr):
+            count=count+1
+            #print "open at "+str(charNum)
+            if(firstChar):
+                firstChar=False
+        charNum=charNum+1
+    return charNum-1
+*/
+
+function matchParens(text, start, openStr, closeStr) {
+  count = 0;
+  charNum = start;
+  firstChar = true;
+  while (count > 0 || firstChar) {
+    if (charNum >= text.length) {
+      console.log("err:could not find match!");
+      return -1;
+    } else if (text.charAt(charNum) == closeStr) {
+      count -= 1;
+      console.log("close at " + String(charNum));
+    } else if (text.charAt(charNum) == openStr) {
+      count += 1;
+      console.log("open at " + String(charNum));
+      if (firstChar) {
+        firstChar = false;
+      }
+    }
+    charNum += 1;
+  }
+  return charNum - 1;
+}
